4b02f01c10ca7d23c827e3ddd2bc87456d52b562
shaz267 <marin.bourdon2@etu.univ-lorraine.fr>
2023-05-15 15:53:31 +0200

    ajout du dossier test

---
+++ untitled/src/test/TestChargeurMagasin.java
@@ -0,0 +1,36 @@
+package test;
+
+import XML.ChargeurMagasin;
+import donnees.Magasin;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TestChargeurMagasin {
+
+    @Test
+    /**
+     * tester que le chargement fonctionne bien quand le répertoire existe
+     */
+    public void testChargerMagasinOK() throws IOException{
+        //preparation des donnees
+        ChargeurMagasin chargeurmagasin = new ChargeurMagasin("../musicbrainzSimple");
+        //methode testee
+        Magasin m = chargeurmagasin.chargerMagasin();
+        //verifications
+        int nombre = ((Magasin) m).getNombreCds();
+        assertEquals("magasin devrait avoir 12 CDs", 12, nombre);
+    }
+
+    @Test(expected = IOException.class)
+    /**
+     * tester que le chargement leve IOException quand repertoire inexistant
+     */
+    public void testChargerMagasinInconnu() throws IOException{
+        //preparation des donnees
+        ChargeurMagasin chargeurmagasin = new ChargeurMagasin("../magasinCD_donnees/X");
+    }
+
+}
